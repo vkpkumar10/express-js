db46401e5a47dd3b98c04fea825f5c50d24b555c
pradeep <vkpkumar@hotmail.com>
2026-09-02 18:46:47 -0400

    adding params

--- src/index.mjs
+++ src/index.mjs
@@ -2,8 +2,34 @@ import express from "express";
 
 const app = express();
 
+const mockusers = [
+  { id: 1, username: "Alex", displayname: "Alex" },
+  { id: 2, username: "Anoop", displayname: "Anoop" },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).send("Hello World!");
+});
+app.get("/api/users", (req, res) => {
+  // res.status(200).json({ message: "Fetching API users" });
+  res.send(mockusers);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = mockusers.find((u) => u.id === userId);
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  res.send(user);
+});
+
+app.get("/api/products", (req, res) => {
+  // res.status(200).json({ message: "Fetching API users" });
+  res.send([
+    { id: 1, productname: "Copper", Price: 10.99 },
+    { id: 2, productname: "Silver", Price: 20.99 },
+  ]);
 });
 
 const PORT = process.env.PORT || 3000;
